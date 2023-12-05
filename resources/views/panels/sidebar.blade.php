2b73b37d4c82f2ca51ba10c6b4e807e67b4e8224
Manoj Niroshana <140615822+manoj-niroshana-esoft@users.noreply.github.com>
2023-12-05 22:16:42 +0530

    Update sidebar.blade.php

--- resources/views/panels/sidebar.blade.php
+++ resources/views/panels/sidebar.blade.php
@@ -9,7 +9,7 @@ $configData = Helper::applClasses();
       <li class="nav-item mr-auto">
         <a class="navbar-brand" href="dashboard-analytics">
           <div class="brand-logo"></div>
-          <h2 class="brand-text mb-0">Save Planets</h2>
+          <h2 class="brand-text mb-0">Save Planet</h2>
         </a>
       </li>
       <li class="nav-item nav-toggle">
