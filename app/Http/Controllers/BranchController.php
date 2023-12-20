bf2c3297f5a897e480c1711cad64dc83497c6cb3
Prakash <E204182@esoft.academy>
2023-12-20 11:56:23 +0530

    function added

--- app/Http/Controllers/BranchController.php
+++ app/Http/Controllers/BranchController.php
@@ -13,5 +13,27 @@ use Carbon\Carbon;
 
 class BranchController extends Controller
 {
-    
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $breadcrumbs = [
+            ['link' => "dashboard-analytics", 'name' => "Home"], ['name' => "Manage Branch"]
+        ];
+        $branches = Branch::orderBy('branch_id', 'desc')->get();
+        $branches->transform(function ($branch) {
+            $division = Division::where('division_id', $branch->division_id)->first();
+            return [
+                'branch_id' => $branch->branch_id,
+                'division' => $division->name,
+                'name' => $branch->name,
+                'created_at' => Carbon::createFromFormat('Y-m-d H:i:s', $branch->created_at)->format('Y-m-d H:i:s'),
+            ];
+        });
+
+        return view('pages.view-branch', compact(['branches', 'breadcrumbs']));
+    }
 }
