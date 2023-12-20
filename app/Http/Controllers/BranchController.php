1068ab494795661c20a69e5ce9e62a9c505b61cd
Prakash <E204182@esoft.academy>
2023-12-20 11:49:57 +0530

    update branch controller

---
+++ app/Http/Controllers/BranchController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Branch;
+use App\Division;
+use Illuminate\Http\Request;
+use Auth;
+use DB;
+use Validator;
+use App\User;
+use Carbon\Carbon;
+
+class BranchController extends Controller
+{
+    
+}
