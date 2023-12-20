429ccc4d79fc0f9e50a11aaf3bee71f0dc53049e
E204182Prakash <152199394+E204182Prakash@users.noreply.github.com>
2023-12-20 22:23:05 +0530

    Update TrackingController.php

--- app/Http/Controllers/TrackingController.php
+++ app/Http/Controllers/TrackingController.php
@@ -17,6 +17,62 @@ use Illuminate\Support\Facades\Log;
 
 class TrackingController extends Controller
 {
-  
+  public function track_complaints(Request $request)
+    {
+        $breadcrumbs = [
+            ['link' => "dashboard-analytics", 'name' => "Home"], ['name' => "Track Complaint"]
+        ];
+        $tracking_id = $request->id;
+        $complaints = Complaint::where('complaint_id', $tracking_id)->get();
+        $complaints->transform(function ($complaint) {
+            switch ($complaint->complain_type) {
+                case '1':
+                    $complain_type = 'Wildlife';
+                    break;
+                case '2':
+                    $complain_type = 'Forestry';
+                    break;
+                case '3':
+                    $complain_type = 'Environment Crime';
+                    break;
+            }
+            $institution = Institution::where('institution_id', $complaint->institution_id)->first()->name;
+            $reported_by = User::where('u_id', $complaint->u_id)->first(['first_name', 'last_name']);
+            $timeframe = 'From : ' . $complaint->from_date;
+            if ($complaint->from_time != null) {
+                $timeframe .= ' ' . $complaint->from_time;
+            }
+            if ($complaint->to_date != null) {
+                $timeframe .=   ' To : ' . Carbon::createFromFormat('Y-m-d H:i:s', $complaint->to_date)->format('Y-m-d');
+            }
+            if ($complaint->to_time != null) {
+                $timeframe .=   ' ' . $complaint->to_time;
+            }
+            return [
+                'complaint_id' => $complaint->complaint_id,
+                'complain_type' => $complain_type,
+                'complain_status' =>  $complaint->complain_status,
+                'institution' => $institution,
+                'description' => $complaint->description,
+                'timeframe' => $timeframe,
+                'reported_by' => $reported_by->first_name . ' ' . $reported_by->last_name,
+            ];
+        });
+        $tracking = ComplaintStatus::where('complaint_id', $tracking_id)->orderBy('status', 'asc')->get();
+        $tracking->transform(function ($trac) {
+            return [
+                'complaint_id' => $trac->complaint_id,
+                'status' => $trac->status,
+                'officer_id' =>  $trac->officer_id,
+                'created_at' => Carbon::createFromFormat('Y-m-d H:i:s', $trac->created_at)->format('D, M d'),
+            ];
+        });
+
+        return view('/pages/track-complaint', [
+            'breadcrumbs' => $breadcrumbs,
+            'complaint' => $complaints[0],
+            'tracking_details' => $tracking,
+        ]);
+    }
   
 }
