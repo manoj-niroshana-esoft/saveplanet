79644eb0d37382f32fba1895826ab2f6ef4f1fca
Manoj Niroshana <140615822+manoj-niroshana-esoft@users.noreply.github.com>
2023-12-20 22:57:22 +0530

    Add Soft delete

--- app/Division.php
+++ app/Division.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Division extends Model
 {
+    use SoftDeletes;
     protected $table = 'division';
     public $primaryKey = 'division_id';
     public $timestamps = true;
