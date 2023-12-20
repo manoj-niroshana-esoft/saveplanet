37a086c44e820029f09125f5dec9a0c292d4b87f
Manoj Niroshana <140615822+manoj-niroshana-esoft@users.noreply.github.com>
2023-12-20 19:44:43 +0530

    Add SoftDeletes to models

--- app/ComplaintDetail.php
+++ app/ComplaintDetail.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ComplaintDetail extends Model
 {
+    use SoftDeletes;
+
     protected $table = 'complaint_detail';
     public $primaryKey = 'complaint_detail_id';
     public $timestamps = true;
